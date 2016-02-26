c6ca7656bb9f8360de77d81703275944af0c9a35
awellsabr <awells@abrinc.com>
2016-02-26 08:09:33 -0900

    this is a test commit

---
+++ py_assign_5_1.py
@@ -0,0 +1,12 @@
+# Using Python version 3.5.0, raw_input no longer valid in this version
+hrs = input('Enter Hours:\n')
+rate = input('Enter pay rate (dollars/hour):')
+r = float(rate)
+h = float(hrs)
+if 0<h<=40:
+    pay_ltet40=h*r
+    print(pay_ltet40)
+else:
+    pay_gt40=(40*r)+((h-40)*(r*1.5))
+    print(pay_gt40)
+    
